01a3d2af87614dd110bd25e42f1fc7cf6b5a27ee
Gustavo Costa <79706058+Gustavo-Costa-Code@users.noreply.github.com>
2022-10-10 09:16:20 -0300

    Add files via upload

---
+++ Who are ya/includes/_pontos.php
@@ -0,0 +1,6 @@
+<?php 
+$pontos = array(
+    0 => array('nome'=>'Praca da Matriz', 'localidade'=>'Centro Histórico', 'ano'=>'1753', 'tamanho'=>'Indefinido', 'tipo'=>'Praca');
+    0 => array('nome'=>'Praca da Matriz', 'localidade'=>'Centro Histórico', 'ano'=>'1753', 'tamanho'=>'Indefinido', 'tipo'=>'Praca');
+)
+?>
