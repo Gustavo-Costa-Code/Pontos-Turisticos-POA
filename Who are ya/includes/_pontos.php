bb2cfa2338f6b58187c917310b45a21cf23cb55b
Gustavo Costa <79706058+Gustavo-Costa-Code@users.noreply.github.com>
2022-10-22 11:24:12 -0300

    Delete Who are ya directory

--- Who are ya/includes/_pontos.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$pontos = array(
-    0 => array('nome'=>'Praca da Matriz', 'localidade'=>'Centro Histórico', 'ano'=>'1753', 'tamanho'=>'Indefinido', 'tipo'=>'Praca');
-    0 => array('nome'=>'Praca da Matriz', 'localidade'=>'Centro Histórico', 'ano'=>'1753', 'tamanho'=>'Indefinido', 'tipo'=>'Praca');
-)
-?>
